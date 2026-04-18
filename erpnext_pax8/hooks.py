4fc93495cea4cd6185a1533f8c10e2b4f2026844
Omar Elmohri <omar.elmohri@gmail.com>
2026-04-18 10:27:01 +0100

    fix: doctype_js path should not include public/ prefix

--- erpnext_pax8/hooks.py
+++ erpnext_pax8/hooks.py
@@ -9,7 +9,7 @@ app_version = "1.0.0"
 required_apps = ["frappe", "erpnext"]
 
 doctype_js = {
-    "Pax8 Settings": "public/js/pax8_settings.js",
+    "Pax8 Settings": "js/pax8_settings.js",
 }
 
 scheduler_events = {
